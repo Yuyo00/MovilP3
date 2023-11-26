d652bd4df69cee76a6a4102816513025f2417580
Antonia Mardones <ant.mardones@duocuc.cl>
2023-11-26 17:04:20 -0300

    Registro con validacion

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { showAlertDUOC, showToast } from '../../model/message';
 import { Usuario } from '../../model/usuario';
@@ -33,7 +33,7 @@ export class RegistroPage implements OnInit {
   public repeticionPasswordValue: any;
   public contraseñaValue: any;
 
-  constructor(private router: Router, private authService : AuthService, private bd: DataBaseService ) {
+  constructor(private router: Router, private authService : AuthService, private bd: DataBaseService, private toastController: ToastController ) {
     this.usuario = new Usuario();
   }
 
@@ -43,6 +43,16 @@ export class RegistroPage implements OnInit {
   public login() {
     this.router.navigate(['/login'])
   }
+
+  async mostrarToast(mensaje: string, color: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000, 
+      color: color
+    });
+    toast.present();
+  }
+  
 
   async crearPerfil() {
     
@@ -62,15 +72,67 @@ export class RegistroPage implements OnInit {
     this.repeticionPasswordValue = this.repeticionPassword.value;
     this.contraseñaValue = this.contraseña.value;
 
-    console.log(this.nombreValue)
-    console.log(this.apellidoValue)
+    let len1 = this.contraseñaValue.length;
+    let len2 = this.contraseñaValue.length;
 
-    if (this.contraseñaValue !== this.repeticionPasswordValue) {
-      showAlertDUOC(`Las contraseñas escritas deben ser iguales.`);
+    const emailRegex = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+
+    if (this.nombreValue == '') {
+      this.mostrarToast('El campo Nombre no puede estar vacio', 'danger');
       return;
     }
+    if (this.apellidoValue == '') {
+      this.mostrarToast('El campo Apellido no puede estar vacio', 'danger');
+      return;
+    }
+
+    if (this.correoValue == '') {
+      this.mostrarToast('El campo Correo no puede estar vacio', 'danger');
+      return;
+    }
+    if (!emailRegex.test(this.correoValue)) {
+      this.mostrarToast('El formato del correo es incorrecto', 'danger');
+      return;
+    }
+
+
+
+    if (this.preguntaValue == '') {
+      this.mostrarToast('El campo Pregunta Secreta no puede estar vacio', 'danger');
+      return;
+    }
+    if (this.respuestaValue == '') {
+      this.mostrarToast('El campo Respuesta Secreta no puede estar vacio', 'danger');
+      return;
+    }
+
+
+    if (this.contraseñaValue == '') {
+      this.mostrarToast('El campo Contraseña no puede estar vacio', 'danger');
+      return;
+    }
+    if (this.repeticionPasswordValue == '') {
+      this.mostrarToast('El campo Repita su Contraseña no puede estar vacio', 'danger');
+      return;
+    }
+    if (len1 < 4) {
+      this.mostrarToast('Laa contraseña debe ser más de 4 caracteres', 'danger');
+      return;
+    }
+    if (len2 < 4) {
+      this.mostrarToast('Laa contraseña debe ser más de 4 caracteres', 'danger');
+      return;
+    }
+    if (this.contraseñaValue !== this.repeticionPasswordValue) {
+      this.mostrarToast('Las contraseñas no coinciden', 'danger');
+      return;
+    }
+
+
+
       await this.bd.crearUsuario(this.correoValue, this.contraseñaValue, this.nombreValue, this.apellidoValue, this.preguntaValue, this.respuestaValue, 'N')
       showToast('Su cuenta fue creada');
+      this.router.navigate(['/login']);
   }
 }
 
